8eb211571238e22b7b7631de6a011f73f9b420a3
Sudish <sudishrestha@gmail.com>
2020-03-24 17:37:31 +0545

    latest changes

--- __manifest__.py
+++ __manifest__.py
@@ -11,11 +11,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale'],
+   'depends':  ['base','sale','sales_team', 'account', 'procurement', 'report', 'web_tour'],
 
     # always loaded
     'data': [
-        'views/reporting_menu.xml'
+        'views/reporting_menu.xml',
+         'report/sale_report_views.xml',
+        'report/sale_report_templates.xml',
+        'report/invoice_report_templates.xml',
+
        
    
     ],
