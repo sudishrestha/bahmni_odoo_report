357c6cafee9bcfbf6ac1f8ec4c364acaf502e66d
Sudish <sudishrestha@gmail.com>
2020-03-26 10:46:24 +0545

    latest push

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from . import sale_analysis
+import report
 #import report
 
 
--- report/__init__.py
+++ report/__init__.py
@@ -1,4 +1,2 @@
 # -*- coding: utf-8 -*-
-import claim_report
-import claim_patient_report
 import insurance_claim_summary_report
